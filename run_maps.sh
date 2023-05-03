c2f871ac98a052176693aac19591ac48fdb13c46
jasmine tan <jtan25@cmc.edu>
2023-05-02 20:19:23 -0700

    please i hope this worked

--- run_maps.sh
+++ run_maps.sh
@@ -1,4 +1,4 @@
-#!/bin/bas#!/bin/bash
-for file in /data/Twitter\ dataset/geoTwitter20-*; do
-	nohup python3 ./src/map.py --input_path="$file" &
+#!/bin/bash
+for file in '/data/Twitter dataset/'geoTwitter20-*.zip; do
+	nohup ./src/map.py --input_path="$file" &
 done
